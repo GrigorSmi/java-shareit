de0f92f5c096cd0037b265ac67beda7ccde6ba63
GrigorSmi <grigor.smi.smirnov@yandex.ru>
2026-09-04 18:49:47 +0300

    DTO + Mapper

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,7 +1,7 @@
-package ru.practicum.shareit.item.dto;
+package ru.practicum.shareit.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,15 +9,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ItemDto {
+public class UserDto {
     private Long id;
 
     @NotBlank
     private String name;
 
     @NotBlank
-    private String description;
-
-    @NotNull
-    private Boolean available;
+    @Email
+    private String email;
 }
